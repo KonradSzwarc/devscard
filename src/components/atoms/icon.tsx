b3ac4b984f5d5eba6127bbe5a4cdb7b294b21d3b
hoolek77 <szymon.kin@gmail.com>
2022-10-22 23:52:20 +0200

    Add url handling to Icon component

--- src/components/atoms/icon.tsx
+++ src/components/atoms/icon.tsx
@@ -6,9 +6,22 @@ export interface IconProps {
   name?: IconName;
   color?: string;
   size: number;
+  url?: string;
 }
 
-const Icon = ({ name = undefined, color = undefined, size }: IconProps) =>
+const BaseIcon = ({ name = undefined, color = undefined, size }: Omit<IconProps, 'url'>) =>
   name ? <IconComponent icon={name} width={size} height={size} style={{ color }} /> : null;
+
+const Icon = ({ url = undefined, ...baseIconProps }: IconProps) => {
+  if (url) {
+    return (
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <BaseIcon {...baseIconProps} />
+      </a>
+    );
+  }
+
+  return <BaseIcon {...baseIconProps} />;
+};
 
 export default Icon;
