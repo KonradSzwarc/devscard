fa06c04bd066680727d94ad03e07eca611a0d8b9
hoolek77 <szymon.kin@gmail.com>
2022-10-23 16:46:58 +0200

    Revert changes to Icon component

--- src/components/atoms/icon.tsx
+++ src/components/atoms/icon.tsx
@@ -6,22 +6,9 @@ export interface IconProps {
   name?: IconName;
   color?: string;
   size: number;
-  url?: string;
 }
 
-const BaseIcon = ({ name = undefined, color = undefined, size }: Omit<IconProps, 'url'>) =>
+const Icon = ({ name = undefined, color = undefined, size }: Omit<IconProps, 'url'>) =>
   name ? <IconComponent icon={name} width={size} height={size} style={{ color }} /> : null;
-
-const Icon = ({ url = undefined, ...baseIconProps }: IconProps) => {
-  if (url) {
-    return (
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        <BaseIcon {...baseIconProps} />
-      </a>
-    );
-  }
-
-  return <BaseIcon {...baseIconProps} />;
-};
 
 export default Icon;
