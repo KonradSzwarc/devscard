5b4ded74199008631838402508a0b083756f2ebb
Marcin Woś <marcin.wos@coderscrew.pl>
2022-10-31 16:06:59 +0100

    replaced icon type lib

--- src/types/icon.ts
+++ src/types/icon.ts
@@ -1,12 +1,3 @@
-import type { StringKeyOf, ValueOf } from 'type-fest';
+import type { CircleFlags, Fa6Brands, Fa6Solid, SimpleIcons } from 'iconify-icon-names';
 
-type IconSets = {
-  'fa6-brands': typeof import('@iconify/json/json/fa6-brands.json');
-  'fa6-solid': typeof import('@iconify/json/json/fa6-solid.json');
-  'simple-icons': typeof import('@iconify/json/json/simple-icons.json');
-  'circle-flags': typeof import('@iconify/json/json/circle-flags.json');
-};
-
-export type IconName = ValueOf<{
-  [IconSet in keyof IconSets]: `${IconSet}:${StringKeyOf<IconSets[IconSet]['icons']>}`;
-}>;
+export type IconName = Fa6Brands | Fa6Solid | SimpleIcons | CircleFlags;
