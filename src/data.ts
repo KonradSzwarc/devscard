e6bb39e3b33bb05ad5bfa4bed45ff40865e75adb
Szymon Kin <68154191+hoolek77@users.noreply.github.com>
2022-11-12 22:15:56 +0100

    Create tooltip component (#77)

--- src/data.ts
+++ src/data.ts
@@ -9,6 +9,7 @@ const data: Data = {
   main: {
     config: {
       icon: 'fa6-solid:user',
+      title: 'Profile',
     },
     image: import('@/assets/my-image.jpeg'),
     fullName: 'Mark Freeman',
