55f8dee3150cd31743dcbfc3a221faa552b0aa04
angbur <ag.grala@gmail.com>
2022-11-28 10:17:41 +0100

    dark mode settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     colors: {
       transparent: 'transparent',
