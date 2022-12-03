27e8c9f5d50057d1b4fbb6005b294fa1361d6ed9
hoolek77 <szymon.kin@gmail.com>
2022-12-03 19:21:49 +0100

    Add OG metadata

--- src/types/seo.ts
+++ src/types/seo.ts
@@ -8,4 +8,8 @@ export interface Seo {
    * Description that will be displayed in Google search results. To be fully visible in search results, it should be no longer than 160 characters.
    */
   description: string;
+  /**
+   * Path to the image inside the public folder that will be used as page icon and as a preview image in site links.
+   */
+  image?: string;
 }
