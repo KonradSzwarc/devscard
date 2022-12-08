c3d75deab8e14cf91dd444b5b839b3065c4d3420
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2022-12-08 15:35:16 +0100

    Add Tailwind IntelliSense to objects

--- src/components/atoms/sidebar-item.tsx
+++ src/components/atoms/sidebar-item.tsx
@@ -14,6 +14,12 @@ export interface SidebarItemProps {
 
 export const MAIN_SECTION: Section = 'main';
 
+const classes = /* tw */ {
+  main: 'inline-flex h-10 w-10 items-center justify-center rounded-lg transition',
+  active: 'bg-primary-600 text-white',
+  inactive: 'bg-white text-gray-400 hover:bg-primary-600 hover:text-white dark:bg-gray-800 dark:text-gray-200',
+};
+
 const SidebarItem = ({ section, icon, title = '' }: SidebarItemProps) => {
   const { hash } = useLocation();
   const href = `#${section}`;
@@ -24,13 +30,7 @@ const SidebarItem = ({ section, icon, title = '' }: SidebarItemProps) => {
     <Tooltip content={`${title || section.charAt(0).toUpperCase() + section.slice(1)} section`} placement="left">
       <a
         href={href}
-        className={`inline-flex h-10 w-10 items-center justify-center rounded-lg transition
-        ${
-          active
-            ? 'bg-primary-600 text-white'
-            : 'bg-white text-gray-400 hover:bg-primary-600 hover:text-white dark:bg-gray-800 dark:text-gray-200'
-        }
-        `}
+        className={`${classes.main} ${active ? classes.active : classes.inactive}`}
         aria-current={active ? 'page' : undefined}
         aria-label={`${section} section`}
       >
