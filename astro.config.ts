b420fa2c0cf2eaafd3f6b3158409d6a573625683
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2023-01-17 18:41:40 +0100

    Add Astro Compress

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,11 @@ import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import { visualizer } from 'rollup-plugin-visualizer';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image(), compress()],
   vite: {
     plugins: [visualizer()],
   },
