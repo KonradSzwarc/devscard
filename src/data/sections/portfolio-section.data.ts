42a1267161c7bae03987950227a3d11702c79aec
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2023-01-20 00:23:00 +0100

    Add pdfDetails property to example data

--- src/data/sections/portfolio-section.data.ts
+++ src/data/sections/portfolio-section.data.ts
@@ -36,6 +36,10 @@ const portfolioSectionData = {
         { label: 'Company', value: 'None' },
         { label: 'Category', value: ['Web app', 'Open source'] },
       ],
+      pdfDetails: [
+        { label: 'Demo', value: 'https://golden-bulls-d73jd7.netlify.app', url: '#' },
+        { label: 'Repository', value: 'https://github.com/mark-freeman/golden-bulls', url: '#' },
+      ],
       description:
         'In tristique vulputate augue vel egestas. Quisque ac imperdiet tortor, at lacinia ex. Duis vel ex hendrerit, commodo odio sed, aliquam enim. Ut arcu nulla, tincidunt eget arcu eget, molestie vulputate nisi. Nunc malesuada leo et est iaculis facilisis.',
       tagsList: {
@@ -53,6 +57,10 @@ const portfolioSectionData = {
         { label: 'My role', value: ['Front-end Developer', 'Mobile Developer', 'Designer'] },
         { label: 'Company', value: 'Facebook' },
         { label: 'Category', value: ['Web app', 'Mobile app'] },
+      ],
+      pdfDetails: [
+        { label: 'Demo', value: 'https://tru-quest-ck7ea3.netlify.app', url: '#' },
+        { label: 'Repository', value: 'https://github.com/mark-freeman/tru-quest', url: '#' },
       ],
       description:
         'Ut ultricies tortor at sodales aliquam. Vivamus metus ante, fringilla nec ligula in, suscipit rhoncus mauris. Praesent hendrerit velit odio, at accumsan urna faucibus convallis. Nunc at massa eget ligula volutpat dictum a sit amet libero. Vestibulum iaculis molestie maximus. In hac habitasse platea dictumst.',
@@ -72,6 +80,10 @@ const portfolioSectionData = {
         { label: 'Company', value: 'None' },
         { label: 'Category', value: ['Web app', 'Open source'] },
       ],
+      pdfDetails: [
+        { label: 'Demo', value: 'https://software-chasers-e82l8e.netlify.app', url: '#' },
+        { label: 'Repository', value: 'https://github.com/mark-freeman/software-chasers', url: '#' },
+      ],
       description:
         'Quisque id consectetur eros. In hac habitasse platea dictumst. Sed eu pulvinar orci. Mauris consequat, est in dignissim varius, neque nisl commodo mauris, id blandit risus justo eu nulla.',
       tagsList: {
@@ -89,6 +101,10 @@ const portfolioSectionData = {
         { label: 'My role', value: 'Front-end Developer' },
         { label: 'Company', value: 'Google' },
         { label: 'Category', value: ['Mobile app', 'Open source'] },
+      ],
+      pdfDetails: [
+        { label: 'Demo', value: 'https://disco-ninjas-g321ol.netlify.app', url: '#' },
+        { label: 'Repository', value: 'https://github.com/mark-freeman/disco-ninjas', url: '#' },
       ],
       description:
         'Praesent eu neque tortor. Vestibulum ac magna nisl. Vivamus massa sem, feugiat in pharetra non, convallis egestas purus. Ut consequat ullamcorper sem, in euismod nibh posuere ut. ',
