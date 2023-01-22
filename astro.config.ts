a201528a9776ec3e2759f47eba87c6d1122345ef
angbur <41025473+angbur@users.noreply.github.com>
2023-01-22 14:38:58 +0100

    Add opt-in Google Analitics integration (#165)

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,18 @@
 import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), compress()],
+  integrations: [
+    tailwind(),
+    image(),
+    compress(),
+    partytown({ config: { forward: ['dataLayer.push', 'clarity.push'] } }),
+  ],
   vite: {
     plugins: [visualizer()],
   },
