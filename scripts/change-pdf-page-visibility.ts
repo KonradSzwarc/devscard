3a412751765534f0b3b455719055627106a398b1
hoolek77 <szymon.kin@gmail.com>
2023-01-30 12:03:08 +0100

    Refactor code

--- scripts/change-pdf-page-visibility.ts
+++ scripts/change-pdf-page-visibility.ts
@@ -5,19 +5,15 @@ const args = process.argv.slice(2);
 const INCLUDED_PDF_PAGE_PATH = './src/pages/pdf.astro';
 const EXCLUDED_PDF_PAGE_PATH = './src/pages/_pdf.astro';
 
-const renameFile = async (from: string, to: string) => {
-  await rename(from, to);
-};
-
 const main = async () => {
   if (args[0] === '--exclude' && existsSync(INCLUDED_PDF_PAGE_PATH)) {
-    await renameFile(INCLUDED_PDF_PAGE_PATH, EXCLUDED_PDF_PAGE_PATH);
+    await rename(INCLUDED_PDF_PAGE_PATH, EXCLUDED_PDF_PAGE_PATH);
 
     return;
   }
 
   if (existsSync(EXCLUDED_PDF_PAGE_PATH)) {
-    await renameFile(EXCLUDED_PDF_PAGE_PATH, INCLUDED_PDF_PAGE_PATH);
+    await rename(EXCLUDED_PDF_PAGE_PATH, INCLUDED_PDF_PAGE_PATH);
 
     return;
   }
